12f4dcb45984d0cbea8578a0485ba12de6fdda19
Saibaba Telukunta <saibaba.telukunta@gmail.com>
2013-04-19 04:36:25 +0000

    Finished work

--- test/main.js
+++ test/main.js
@@ -1,11 +1,29 @@
 var should = require("should");
 var sleepsort = require("../lib/main");
 
-describe('sleepsort', function() {
-    describe('with no arguments', function() {
-        it('returns an empty array', function() {
-           var result = sleepsort();
+describe('with an empty array argument', function() {
+    it('calls the callback with an empty array', function(done) {
+       var result = sleepsort([], function(result) {
            result.should.eql([]);
+           done();
+       });
+    });
+});
+
+describe('with a single element array', function() {
+    it('calls the callback with a single element array', function(done) {
+        var result = sleepsort([1], function(result) {
+            result.should.eql([1]);
+            done();
+        });
+    });
+});
+
+describe('with an unsorted two element array', function() {
+    it('calls the callback with a sorted two element array', function(done) {
+        var result = sleepsort([2, 1], function(result) {
+            result.should.eql([1, 2]);
+            done();
         });
     });
 });
